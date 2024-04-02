0986288bd4dfe701dd6bd1f6a74e14db91ab4308
Théo <masselot.theo.59@gmail.com>
2024-04-02 10:02:28 +0200

    TP3

---
+++ TP3/js/main.js
@@ -0,0 +1,6 @@
+'use strict';
+/* eslint-env browser, es6 */
+
+// Pas besoin d'évenement window.onload puisqu'on utilise l'attribut defer
+// lorsque l'on charge notre script
+// console.log("Page load après");
