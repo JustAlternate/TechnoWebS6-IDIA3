215554e874473de25f0849ab28ab5276d708d42e
JustAlternate <lololollololol890@gmail.com>
2024-04-03 20:53:50 +0200

    going further into TP2 (nearly done)

--- TP3/js/main.js
+++ TP3/js/main.js
@@ -14,11 +14,7 @@ function updateSelector(data){
         opt.text = data[i].name;
         select.add(opt);
     }
-    
-
-
 }
-
 function loadGenres() {
     fetch('http://localhost:3000/genres', { method: 'GET' })
     .then(response => {
@@ -38,7 +34,6 @@ function loadGenres() {
     })
     .catch(error => console.log('error:', error));
 }
-
 async function updateArtists(genre) {
     const response = await fetch('http://localhost:3000/genres/'+genre+'/artists', { method: 'GET' })
     const artists = await response.json();
@@ -47,25 +42,23 @@ async function updateArtists(genre) {
     const ul = document.querySelector("#main ul");
     const ul_li_childs = ul.querySelectorAll("li");
     ul_li_childs.forEach(element => {
-        element.remove();
+        //TODO : try to find a more efficient and prettty looking way than using a forEach to wipe ul children.
+        element.remove(); // We remove any left li inside our ul.
     })
-
-
     artists.forEach(element => {
         const li = document.createElement("li");
         const h3 = document.createElement("h3");
         const img = document.createElement("img");
+        h3.addEventListener('click', (evt) => {artistSelected(evt)});
 
         h3.textContent = element.name;
+        h3.id = element.id; // Put the id of the artist inside h3.id for the popup.
         img.src = element.photo;
         li.appendChild(h3);
         li.appendChild(img);
         ul.appendChild(li);
     });
-
 }
-
-
 function loadArtists(data, genre_id){
 
     const h2 = document.querySelector("#main").querySelector("h2");
@@ -75,8 +68,15 @@ function loadArtists(data, genre_id){
     p.textContent = data[genre_id].description;
 
     updateArtists(data[genre_id].id);
-
-
+}
+async function artistSelected(evt){
+  let artist_id = evt.target.id; // Use the id attribut inside h3 to find the artist id.
+  let response = await fetch('http://localhost:3000/artists/'+artist_id+'/albums', { method: 'GET' })
+  let albums = await response.json();
+  let popup = document.querySelector('aside'); 
+  popup.style.visibility = "visible";
+  popup.style.opacity = 0.9;
+  popup.style.transform = 'translateX(350px)';
 }
 
 loadGenres();
