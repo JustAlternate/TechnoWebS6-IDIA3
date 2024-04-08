b9a4a74e685d6118e7607fb619f4336294ff060c
Théo <masselot.theo.59@gmail.com>
2024-04-08 14:58:47 +0200

    Fin TP3

--- TP3/js/main.js
+++ TP3/js/main.js
@@ -1,18 +1,29 @@
 'use strict';
 /* eslint-env browser, es6 */
+
+/* updateSelector func :
+- args : data (json data requested for genres)
+- Used to provide a selection of different genres */
 function updateSelector(data){
     const select = document.querySelector('#main select');
-    for (let i = 0; i < data.length; i += 1){
+    console.log('data', data);
+    data.forEach((genre, i) => {
+        // We use opt = option
         const opt = document.createElement('option');
-        opt.value = i; // opt = option
-        opt.text = data[i].name;
+        opt.value = i;
+        opt.text = genre.name;
         select.add(opt);
-    }
+    });
 }
-// Function using the fetch and .then .data syntax.
+
+/* loadGenres func :
+- args : None
+- Function that load the genres and that allows to
+update the page with the corresponding artists
+- It is using the fetch and .then .data syntax.*/
 function loadGenres() {
     fetch('http://localhost:3000/genres', {method: 'GET'})
-        .then(response => {
+        .then(response => { // We check the response
             if (response.ok){
                 return response.json();
             }
@@ -25,6 +36,7 @@ function loadGenres() {
         .then((data) => {
             updateSelector(data);
             document.querySelector('#main select').addEventListener('change', (evt) => {
+                // We are ad
                 loadArtists(data, evt.target.value);
             });
             // By default we load the first option of the select element.
@@ -33,6 +45,10 @@ function loadGenres() {
         .catch(error => console.log('error:', error));
 }
 
+/* updateArtists func :
+- args : genre (str)
+- Function that request the artists for a specific genre
+update the page with the corresponding artists */
 async function updateArtists(genre) {
     const response = await fetch('http://localhost:3000/genres/' + genre + '/artists', {method: 'GET'});
     const artists = await response.json();
@@ -59,6 +75,9 @@ async function updateArtists(genre) {
     ul.replaceChildren(...new_children_to_add);
 }
 
+/* loadArtists func :
+- args : None
+- Function that load the different artists for a genre*/
 function loadArtists(data, genre_id){
     const h2 = document.querySelector('#main').querySelector('h2');
     const p = document.querySelector('#main > p');
@@ -69,6 +88,11 @@ function loadArtists(data, genre_id){
 
     updateArtists(genre_name);
 }
+
+/* artistSelected func :
+- args : evt(event)
+- Function that load the different albums for an artist in
+a popup window*/
 async function artistSelected(evt){
     const artist_id = evt.target.id; // Use the id attribut inside h3 to find the artist id.
     const  response = await fetch('http://localhost:3000/artists/' + artist_id + '/albums', {
@@ -80,41 +104,38 @@ async function artistSelected(evt){
     const alb_atts = ['cover', 'title', 'year', 'label'];
 
     // if tbody is empty
-   if (!!(table_v.children)){
-      table_v.replaceChildren();
+    if (!!(table_v.children)){
+        table_v.replaceChildren();
     }
-
-    for (let num_alb = 0; num_alb < albums.length; num_alb += 1){
+    albums.forEach((album) => {
         const tr = document.createElement('tr');
-        for (let att = 0; att < alb_atts.length; att += 1){
+        alb_atts.forEach((att) => {
             const td = document.createElement('td');
-            if (alb_atts[att] === 'cover'){
+            if (att === 'cover'){
                 const img = document.createElement('img');
-                img.src = albums[num_alb][alb_atts[att]];
+                img.src = album[att];
                 td.appendChild(img);
             }
             else {
-                td.textContent = albums[num_alb][alb_atts[att]];
+                td.textContent = album[att];
             }
             tr.appendChild(td);
-        }
+        });
         table_v.appendChild(tr);
     }
+    );
     // launch the animation of appearing
-    popup.style.transform = 'translateX(350px)';
-    popup.style.opacity = '1';
-    popup.style.visibility = 'visible';
+    popup.classList.toggle('show');
 
     // EventListener to make the popup vanish
-    popup.querySelector("button").addEventListener('click', (evt) => {
-      popup.style.transform = 'translateX(750px)';
-      popup.style.opacity = '0';
-      popup.style.visibility = 'hidden';
-      setTimeout(() => {
-        const tr = table_v.querySelector('tr');
-        tr.replaceChildren();
-      }, "1000");
-      popup.style.transform = 'translateX(-750px)';
+    popup.querySelector('button').addEventListener('click', () => {
+
+        popup.classList.remove('show');
+
+        setTimeout(() => {
+            const tr = table_v.querySelector('tr');
+            tr.replaceChildren();
+        }, '1000');
     });
 }
 
