33ed9e5ca5262beadfe3fbbd8e4132beb7adc407
Théo <masselot.theo.59@gmail.com>
2024-04-12 18:02:49 +0200

    Adding bonus

--- TP4/Exo3/api/api.js
+++ TP4/Exo3/api/api.js
@@ -33,17 +33,64 @@ app.get('/genres', async function (_, res) {
         return data_as_dict;
 
     });
-    const dict = {}
+    let list = []
     Promise.all(promises)
       .then((values) => {
           values.forEach(element => {
-              dict[element[0].name] = element[0].wiki.summary;
+              list.push({
+                'id':element[0].name,
+                'name':element[0].name,
+                'description':element[0].wiki.summary})
+                ;
           });
-          return res.json(dict);
+          return res.json(list);
       })
       .catch((error) => {
           return res.status(404).send('Error when fetching summaries');
       });
+});
+
+
+app.get('/genre/*', async function (req, res) {
+    const response_genres = await fetch('http://ws.audioscrobbler.com/2.0/?method=tag.getTopTags&api_key='+API_KEY+'&format=json', {
+            method: 'GET',
+            mode: 'cors',
+            headers: { 'Content-Type': 'application/json' }
+        });
+    const data = await response_genres.json();
+    let genres = [];
+
+    data.toptags.tag.forEach((genre) => {
+        genres.push(genre.name);
+    })
+
+
+
+    let args = req.params[0].split('/');
+
+    if (args.length > 1 && genres.includes(args[0]) && (args[1] === 'artists')) {
+        res.set('Content-Type', 'application/json; charset=utf-8');
+        let artists = [];
+        const response = await fetch('http://ws.audioscrobbler.com/2.0/?method=tag.gettopartists&tag='+args[0]+'&api_key='+API_KEY+'&format=json', {
+            method: 'GET',
+            mode: 'cors',
+            headers: { 'Content-Type': 'application/json' }
+        });
+        const artists_data = await response.json();
+        artists_data.topartists.artist.forEach((artist) => {
+            artists.push({
+                'name':artist.name,
+                'genreId':args[0],
+                'id':artist.name,
+                'photo':artist.image[0]['#text']
+            });
+        });
+        
+        res.send(artists);
+    }
+    else {
+        res.status(404).send('Not Found !');
+    }
 });
 
 // export de notre application vers le serveur principal
