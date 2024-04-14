4e256606db5bdb6dfdfa82d2050650d75cfb45ee
Théo <masselot.theo.59@gmail.com>
2024-04-14 12:48:12 +0200

    Fixing linter

--- TP4/Exo3/api/api.js
+++ TP4/Exo3/api/api.js
@@ -5,13 +5,13 @@ let express = require('express');
 let fetch = require('node-fetch');
 let app = express();
 
-const API_KEY = 'ca6a5cfa73124b89d760d1e75f993b01'
+const API_KEY = 'ca6a5cfa73124b89d760d1e75f993b01';
 
 app.get('/genres', async function (_, res) {
-    const response = await fetch('http://ws.audioscrobbler.com/2.0/?method=tag.getTopTags&api_key='+API_KEY+'&format=json', {
+    const response = await fetch('http://ws.audioscrobbler.com/2.0/?method=tag.getTopTags&api_key=' + API_KEY + '&format=json', {
         method: 'GET',
         mode: 'cors',
-        headers: { 'Content-Type': 'application/json' }
+        headers: {'Content-Type': 'application/json'},
     });
     const data = await response.json();
     let genres = [];
@@ -22,10 +22,10 @@ app.get('/genres', async function (_, res) {
 
     let promises = genres.map(async function (item) {
 
-        const response = await fetch('http://ws.audioscrobbler.com/2.0/?method=tag.getinfo&tag='+item+'&api_key='+API_KEY+'&format=json', {
+        const response = await fetch('http://ws.audioscrobbler.com/2.0/?method=tag.getinfo&tag=' + item + '&api_key=' + API_KEY + '&format=json', {
             method: 'GET',
             mode: 'cors',
-            headers: { 'Content-Type': 'application/json' }
+            headers: {'Content-Type': 'application/json'},
         });
 
         const data = await response.json();
@@ -33,36 +33,35 @@ app.get('/genres', async function (_, res) {
         return data_as_dict;
 
     });
-    let list = []
+    let list = [];
     Promise.all(promises)
-      .then((values) => {
-          values.forEach(element => {
-              list.push({
-                'id':element[0].name,
-                'name':element[0].name,
-                'description':element[0].wiki.summary})
-                ;
-          });
-          return res.json(list);
-      })
-      .catch((error) => {
-          return res.status(404).send('Error when fetching summaries');
-      });
+        .then((values) => {
+            values.forEach(element => {
+                list.push({
+                    'id': element[0].name,
+                    'name': element[0].name,
+                    'description': element[0].wiki.summary});
+            });
+            return res.json(list);
+        })
+        .catch(() => {
+            return res.status(404).send('Error when fetching summaries');
+        });
 });
 
 
 app.get('/genre/*', async function (req, res) {
-    const response_genres = await fetch('http://ws.audioscrobbler.com/2.0/?method=tag.getTopTags&api_key='+API_KEY+'&format=json', {
-            method: 'GET',
-            mode: 'cors',
-            headers: { 'Content-Type': 'application/json' }
-        });
+    const response_genres = await fetch('http://ws.audioscrobbler.com/2.0/?method=tag.getTopTags&api_key=' + API_KEY + '&format=json', {
+        method: 'GET',
+        mode: 'cors',
+        headers: {'Content-Type': 'application/json'},
+    });
     const data = await response_genres.json();
     let genres = [];
 
     data.toptags.tag.forEach((genre) => {
         genres.push(genre.name);
-    })
+    });
 
 
 
@@ -71,21 +70,20 @@ app.get('/genre/*', async function (req, res) {
     if (args.length > 1 && genres.includes(args[0]) && (args[1] === 'artists')) {
         res.set('Content-Type', 'application/json; charset=utf-8');
         let artists = [];
-        const response = await fetch('http://ws.audioscrobbler.com/2.0/?method=tag.gettopartists&tag='+args[0]+'&api_key='+API_KEY+'&format=json', {
+        const response = await fetch('http://ws.audioscrobbler.com/2.0/?method=tag.gettopartists&tag=' + args[0] + '&api_key=' + API_KEY + '&format=json', {
             method: 'GET',
             mode: 'cors',
-            headers: { 'Content-Type': 'application/json' }
+            headers: {'Content-Type': 'application/json'},
         });
         const artists_data = await response.json();
         artists_data.topartists.artist.forEach((artist) => {
             artists.push({
-                'name':artist.name,
-                'genreId':args[0],
-                'id':artist.name,
-                'photo':artist.image[0]['#text']
+                'name': artist.name,
+                'genreId': args[0],
+                'id': artist.name,
+                'photo': artist.image[0]['#text'],
             });
         });
-        
         res.send(artists);
     }
     else {
